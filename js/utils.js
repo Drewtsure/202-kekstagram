0746f9a49278335b227bb88f503912df92f82667
Andrew Zakharchenko <drewtsure@gmail.com>
2019-01-22 21:03:10 +0300

    прикручивает фильтры

--- js/utils.js
+++ js/utils.js
@@ -9,6 +9,16 @@
     getRandomValue: function (min, max) {
       return Math.floor(Math.random() * (max - min) + min);
     },
+    shuffleArray: function (array) {
+      var temp;
+      for (var i = 0; i < array.length; i++) {
+        var j = Math.floor(window.utils.getRandomValue(i, array.length));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return array;
+    },
     isKeyEsc: function (evt) {
       return evt.keyCode === KeyCodes.ESC;
     }
